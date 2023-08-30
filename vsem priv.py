f73b20cec694bb93ea042203b6468fd909034d9e
Latyshev Pavel zhestki tip <trevor.015@mail.ru>
2023-08-30 17:31:27 +0300

    nomer devyatb

--- vsem priv.py
+++ vsem priv.py
@@ -5,7 +5,19 @@ matrix = [[random.randint(1, 10) for i in range(m)] for j in range(n)]
 print(matrix)
 for line in matrix:
     for element in line:
+        break
         print("%4d" % element, end=' ')
     print()
 print()
-print('\n'.join([' '.join('{:{width}}'.format(element, width=4) for element in line) for line in matrix]))
+
+# функция принимает матрицу, возвращает максимальный элемент среди минимальных элементов столбцов
+def ex_9 (matrix):
+    min_column = []
+    for i in range(m):
+        column = []
+        for j in range(n):
+            column.append(matrix[j][i])
+        min_column.append(min(column))
+    return max(min_column)
+
+print(ex_9(matrix))
